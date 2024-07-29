b8244df92e4cfa468df2206a11381d9252ed79cf
Leo <git@leocatto.com>
2024-07-29 22:27:25 +1000

    fix: update headless paths

--- src/setup_resonite.sh
+++ src/setup_resonite.sh
@@ -6,8 +6,8 @@ bash "${STEAMCMDDIR}/steamcmd.sh" \
 	+app_license_request ${STEAMAPPID} \
 	+app_update ${STEAMAPPID} -beta ${STEAMBETA} -betapassword ${STEAMBETAPASSWORD} validate \
 	+quit
-find ${STEAMAPPDIR}/Data/Assets -type f -atime +7 -delete
-find ${STEAMAPPDIR}/Data/Cache -type f -atime +7 -delete
+find ${STEAMAPPDIR}/Headless/net8.0/Data/Assets -type f -atime +7 -delete
+find ${STEAMAPPDIR}/Headless/net8.0/Data/Cache -type f -atime +7 -delete
 find /Logs -type f -name *.log -atime +30 -delete
 mkdir -p Headless/Migrations
 
--- src/start_resonite.sh
+++ src/start_resonite.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-exec dotnet ${STEAMAPPDIR}/Headless/net8.0/Resonite.dll -HeadlessConfig Config/Config.json -l /Logs $(if {{ENABLE_MODLOADER}}; then echo "-LoadAssembly "./Libraries/ResoniteModLoader.dll""; fi)
+exec dotnet ${STEAMAPPDIR}/Headless/net8.0/Resonite.dll -HeadlessConfig Config/Config.json -l /Logs $(if $ENABLE_MODLOADER; then echo "-LoadAssembly "./Libraries/ResoniteModLoader.dll""; fi)
